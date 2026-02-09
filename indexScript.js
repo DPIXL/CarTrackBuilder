2a9b13894636aa5fdb02a72a5c05e7948d0b0e83
patejdlstepan@sssvt.cz <patejdlstepan@sssvt.cz>
2026-02-09 08:46:16 +0100

    initial 2

--- indexScript.js
+++ indexScript.js
@@ -1,2 +1,11 @@
 'use strict';
 
+function loadView(viewName) {
+    fetch(`Views/${viewName}/index.html`)
+        .then(res => res.text())
+        .then(html => {
+            document.querySelector('.content').innerHTML = html;
+        });
+}
+
+loadView('MainMenu');
