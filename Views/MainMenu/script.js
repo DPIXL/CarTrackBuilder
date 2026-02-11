53bf7f701849f8cd82af405de65f9be616b03e28
Štěpán Václav Patejdl <stepan.vaclav.patejdl@gmail.com>
2026-02-11 17:10:17 +0100

    Smaller changes to the editor, major changes to the Main Menu, added html css and js functionality. Also edited the save/load methods to work with MainMenu.

--- Views/MainMenu/script.js
+++ Views/MainMenu/script.js
@@ -1,5 +1,43 @@
 'use strict';
 
+import {getSavedTrackNames} from "../../Modules/storage.js";
+
 export function init() {
 
+    document.querySelectorAll('.headerbutton').forEach((button) => {
+        button.classList.remove('buttonon');
+    });
+    document.querySelector('.mainmenub').classList.add('buttonon');
+
+    const loadmapsel = document.querySelector('.loadmapselect');
+
+    const newmap = document.querySelector('.newmap');
+    const loadmap = document.querySelector('.loadmap');
+
+    let availablemaps = getSavedTrackNames();
+
+    availablemaps.forEach((item) => {
+        let newoption = document.createElement('option');
+        newoption.text = item;
+        newoption.value = item;
+        loadmapsel.options.add(newoption);
+    });
+
+    newmap.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const trackName = formData.get('newname');
+
+        loadView('Editor', trackName);
+    });
+
+    loadmap.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const trackName = formData.get('selectedtrack');
+
+        loadView('Editor', trackName);
+    });
 }
